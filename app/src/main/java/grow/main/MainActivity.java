79837475091c5a35834c56eb82038a32fd96f4b3
zhangly <zhangly@ushareit.com>
2017-12-06 00:11:21 +0800

    [App] First commit for me.

---
+++ app/src/main/java/grow/main/MainActivity.java
@@ -0,0 +1,15 @@
+package grow.main;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.flow.grow.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ app/src/main/java/grow/main/MainAdapter.java
@@ -0,0 +1,21 @@
+package grow.main;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.ViewGroup;
+
+public class MainAdapter extends RecyclerView.Adapter {
+    @Override
+    public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+}
